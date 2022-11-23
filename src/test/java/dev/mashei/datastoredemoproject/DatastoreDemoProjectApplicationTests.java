7603d3d9d98743eb46bca5641c2671c84391ac1a
nikmashei <nikmashej@gmail.com>
2022-11-23 23:44:43 +0200

    initial commit

---
+++ src/test/java/dev/mashei/datastoredemoproject/DatastoreDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mashei.datastoredemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatastoreDemoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
